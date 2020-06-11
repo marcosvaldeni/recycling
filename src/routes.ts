a35950e1d8ae43d1bdfe4d28d1b80f3039ea651f
Marcos Lucas <marcosvaldeni@gmail.com>
2020-06-11 14:17:17 +0100

    route get items

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/users', (req, res) => {
-  console.log('Listagem');
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
 
-  res.json(['My app']);
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  })
+  return res.json(items);
 });
 
 export default routes;
